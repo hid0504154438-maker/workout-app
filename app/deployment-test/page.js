6a91e0d0da9ec4e9c313688fc7fb4274eb81f943
Hid <hid05@app.com>
2026-02-18 13:59:52 +0200

    chore: add vercel.json and test page

---
+++ app/deployment-test/page.js
@@ -0,0 +1,18 @@
+export default function DeploymentTest() {
+    return (
+        <div style={{
+            backgroundColor: '#00ff00',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: 'black',
+            fontSize: '3rem',
+            fontWeight: 'bold'
+        }}>
+            DEPLOYMENT WORKING
+            <br />
+            Updated: 14:03
+        </div>
+    );
+}
